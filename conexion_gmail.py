e462b1e3382dd4d72b0bf00e8241f1971d76a411
Josehernandez <jhernandeza@fi.uba.ar>
2021-07-03 14:22:46 -0300

    primer push

---
+++ conexion_gmail.py
@@ -0,0 +1,69 @@
+import os
+
+from googleapiclient.discovery import Resource, build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
+
+SCOPES = [
+    'https://www.googleapis.com/auth/gmail.readonly',
+    'https://www.googleapis.com/auth/gmail.send'
+]
+
+# Archivo generado para la API
+ARCHIVO_SECRET_CLIENT = 'client_secret_gmail.json'
+
+
+def cargar_credenciales() -> Credentials:
+    credencial = None
+
+    if os.path.exists('token.json'):
+        with open('token.json', 'r'):
+            credencial = Credentials.from_authorized_user_file('token.json', SCOPES)
+
+    return credencial
+
+
+def guardar_credenciales(credencial: Credentials) -> None:
+    with open('token.json', 'w') as token:
+        token.write(credencial.to_json())
+
+
+def son_credenciales_invalidas(credencial: Credentials) -> bool:
+    return not credencial or not credencial.valid
+
+
+def son_credenciales_expiradas(credencial: Credentials) -> bool:
+    return credencial and credencial.expired and credencial.refresh_token
+
+
+def autorizar_credenciales() -> Credentials:
+    flow = InstalledAppFlow.from_client_secrets_file(ARCHIVO_SECRET_CLIENT, SCOPES)
+
+    return flow.run_local_server(open_browser=False, port=0)
+
+
+def generar_credenciales() -> Credentials:
+    credencial = cargar_credenciales()
+
+    if son_credenciales_invalidas(credencial):
+
+        if son_credenciales_expiradas(credencial):
+            credencial.refresh(Request())
+
+        else:
+            credencial = autorizar_credenciales()
+
+        guardar_credenciales(credencial)
+
+    return credencial
+
+
+def obtener_servicio() -> Resource:
+    """
+    Creador de la conexion a la API Gmail
+    """
+    return build('gmail', 'v1', credentials=generar_credenciales())
+
+obtener_servicio()
