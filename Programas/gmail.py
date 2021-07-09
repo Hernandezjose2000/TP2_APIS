2707212d318bf9b00f0920e9499ca884b18780ec
josehernandez <jhernandeza@fi.uba.ar>
2021-07-09 14:38:41 -0300

    Obteniendo fecha en formato UNIX

--- Programas/gmail.py
+++ Programas/gmail.py
@@ -1,5 +1,6 @@
 import os
-
+import datetime
+import time
 from googleapiclient.discovery import Resource, build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
@@ -70,37 +71,64 @@ def obtener_servicio() -> Resource:
 Todo lo anterior a la variable servicio se encuentra en el archivo conexion_gmail, se debe importar, pero
 estoy teniendo problemas para importarlos, no me reconoce la ruta, lo solucionare.
 '''
+ASUNTO = 21
 
-def almacenando_asuntos(id_mails:list, servicio:Resource):
+def almacenando_asuntos(id_mails:list, servicio:Resource): #evaluar en que campo vendra el asunto
 
     asuntos = [] #Los almacenamos de esta manera ['28345234, TP_1', '789101112, Parcial_2', '123456, Parcial_2_Recuperatorio_1']
+
     for i in id_mails:
-        lectura_mail = servicio.users().messages().get(userId='me', id = i).execute()
-        filtro = lectura_mail['payload']['headers'][19]['value']
-        asuntos.append(filtro)
+        lectura_mail = servicio.users().messages().get(userId='evaluaciontp2@gmail.com', id = i).execute()
+        buscando_asunto = lectura_mail['payload']['headers'][ASUNTO]['value']
+        asuntos.append(buscando_asunto)
 
     print(asuntos)
 
 
-def obteniendo_ids_mails(servicio:Resource) -> list:
+def extrayendo_nombres_alumnos(id_mails:list, servicio:Resource):
+
+    nombres_alumnos = []
+
+    for i in id_mails:
+        lectura_mail = servicio.users().messages().get(userId='evaluaciontp2@gmail.com', id = i).execute()
+        nombre_apellido_alumnos = lectura_mail['snippet']
+        nombres_alumnos.append(nombre_apellido_alumnos)
+
+    print(nombres_alumnos)
+
+
+def obteniendo_ids_mails(servicio:Resource, fecha_actual:float) -> list:
 
     #PRE: No recibimos ningun parametro.
     #POST: Retronamos en una lista los id's de los mails.
     
     id_mails = [] #Se guaradaran asi ['123345', 'dhgfgh34534543']
-    emails_recibidos = servicio.users().messages().list(userId='me', q='is:unread').execute() #falta colocar la query, veremos en base a que se obtienen.
+    emails_recibidos = servicio.users().messages().list(userId='evaluaciontp2@gmail.com', q=f'newer:{fecha_actual}').execute() #falta colocar la query, veremos en base a que se obtienen.
     obteniendo_ids = emails_recibidos['messages']
     
     for id in obteniendo_ids:
         id_mails.append(id['id'])
-    
+    print(id_mails)
     return id_mails
+
+
+def obteniendo_fecha_actual()-> int:
+
+    #PRE: No recibimos nada como argumento
+    #POST: Retornamos la fecha casteada como int en formato UNIX
+
+    fecha = str(datetime.date.today())
+    conversion_unix = int(time.mktime(datetime.datetime.strptime(fecha.replace("-","/"), '%Y/%m/%d').timetuple()))
+
+    return conversion_unix
 
 
 def main():
 
+    fecha = obteniendo_fecha_actual()    
     servicio = obtener_servicio()
-    id_mails = obteniendo_ids_mails(servicio)
+    id_mails = obteniendo_ids_mails(servicio, fecha)
     almacenando_asuntos(id_mails, servicio)
+    #extrayendo_nombres_alumnos(id_mails, servicio)
 
 main()
