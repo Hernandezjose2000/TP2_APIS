96ad58d228e88487811589a276fab41e7454f85b
josehernandez <jhernandeza@fi.uba.ar>
2021-07-12 09:45:40 -0300

    Se agrega la funcionalidad de enviar mail a las entregas correctas e incorrectas

--- Programas/gmail.py
+++ Programas/gmail.py
@@ -13,13 +13,17 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import base64
 
+#constantes
+ASUNTO = 19
+EMAIL = 1
+ARCHIVO_ADJUNTO = 1
+ORIGEN = 16
+ARCHIVO_SECRET_CLIENT = 'client_secret_gmail.json'
+
 SCOPES = [
     'https://www.googleapis.com/auth/gmail.readonly',
     'https://www.googleapis.com/auth/gmail.send'
 ]
-
-# Archivo generado para la API
-ARCHIVO_SECRET_CLIENT = 'client_secret_gmail.json'
 
 
 def cargar_credenciales() -> Credentials:
@@ -77,26 +81,26 @@ def obtener_servicio() -> Resource:
 Todo lo anterior a la variable servicio se encuentra en el archivo conexion_gmail, se debe importar, pero
 estoy teniendo problemas para importarlos, no me reconoce la ruta, lo solucionare.
 '''
-ASUNTO = 19
-EMAIL = 1
-ARCHIVO_ADJUNTO = 1
 
 
 def obteniendo_datos_mails(id_mails:list, servicio:Resource) -> dict:
 
-    datos_emails = {} #Los almacenamos de esta manera ['28345234, TP_1', '789101112, Parcial_2', '123456, Parcial_2_Recuperatorio_1']
+    #PRE: Recibimos la lista con los id de los mails que coinciden con la fecha actual.
+    #POST: Retornamos un diccionario con los datos que requerimos obtener de estos mails enviados.
+
+    datos_emails = {} #{id de mail:{"asunto":asunto, "origen":origen, "id de archivo adjunto":id_archivo_adjunto}}
 
     for id_mail in id_mails:
 
         lectura_mail = servicio.users().messages().get(userId='evaluaciontp2@gmail.com', id = id_mail).execute()
-        obteniendo_origen = lectura_mail['payload']['headers'][16]['value'].split("<")
+        obteniendo_origen = lectura_mail['payload']['headers'][ORIGEN]['value'].split("<")
         email_origen = obteniendo_origen[EMAIL].rstrip(">")
         asunto = lectura_mail['payload']['headers'][ASUNTO]['value'].split("-")
         id_archivo_adjunto = lectura_mail['payload']['parts'][ARCHIVO_ADJUNTO]['body']['attachmentId']
 
         datos_emails[id_mail] = {"asunto":asunto, "origen": email_origen, "adj_id":id_archivo_adjunto}
-    return datos_emails
 
+    return datos_emails
 
 
 def obteniendo_ids_mails(servicio:Resource, fecha_actual:float) -> list:
@@ -105,7 +109,9 @@ def obteniendo_ids_mails(servicio:Resource, fecha_actual:float) -> list:
     #POST: Retronamos en una lista los id's de los mails.
     
     id_mails = [] #Se guaradaran asi ['123345', 'dhgfgh34534543']
-    emails_recibidos = servicio.users().messages().list(userId='evaluaciontp2@gmail.com', q=f'newer: {fecha_actual}').execute()#captar la excepcion de la no conexion al servicio
+    emails_recibidos = servicio.users().messages().list(userId='evaluaciontp2@gmail.com', q=f'newer: {fecha_actual}').execute()
+    #captar la excepcion de la no conexion al servicio
+
     obteniendo_ids = emails_recibidos['messages']
     
     for id in obteniendo_ids:
@@ -126,11 +132,12 @@ def obteniendo_fecha_actual() -> int:
     return conversion_unix
 
 
-def validando_datos_asuntos(id_mails:list, datos_emails:dict):
+def validando_padron_alumnos(id_mails:list, datos_emails:dict, servicio:Resource):
 
     lineas = []
     emails_entregas_incorrectas = []
-    entregas_correctas = []
+    archivos_entregas_correctas = []
+    email_entregas_correctas = []
 
     with open("\\Users\\joseh\\Documents\\algoritmos_y_programacion_1\\TP2_APIS\\Programas\\alumnos.csv", "r") as archivo:
 
@@ -140,37 +147,45 @@ def validando_datos_asuntos(id_mails:list, datos_emails:dict):
         for linea in lectura:
             lineas.append(linea)               
 
-        for j in id_mails:
+        for id_mail in id_mails:
 
             k = 0
             while k < 17: 
-                if datos_emails[j]['asunto'][0].strip(" ") in lineas[k][1]:
-                    entregas_correctas.append(datos_emails[j]['adj_id'])
+                if datos_emails[id_mail]['asunto'][0].strip(" ") in lineas[k][1]:
+                    archivos_entregas_correctas.append(datos_emails[id_mail]['adj_id'])
+                    email_entregas_correctas.append(datos_emails[id_mail]['origen'])
                     k = 17
-                else:
-                    
+
+                else:                  
                     if k < 16:
                         print("validando")
                     else:
-                        emails_entregas_incorrectas.append(datos_emails[j]['origen'])
+                        emails_entregas_incorrectas.append(datos_emails[id_mail]['origen'])
                     k+=1
-        print(emails_entregas_incorrectas)
+
+        enviando_mails(servicio, emails_entregas_incorrectas, asunto="Entrega fallida", cuerpo = "Tu padron no coincide con nuestra base de datos")
+        enviando_mails(servicio, email_entregas_correctas, asunto = "Entrega correcta", cuerpo = "Tu entrega ha sido exitosa")
     
     return emails_entregas_incorrectas
 
-def enviando_mail_entregas_fallidas(servicio:Resource, entregas_incorrectas:list):
 
+def enviando_mails(servicio:Resource, entregas:list, asunto:str, cuerpo:str) -> None:
 
-    for mail in entregas_incorrectas:
+    #PRE: Recibimos una lista con los mails que no coinciden su padron con lo que tenemos e el archivo alumnos.csv.
+    #POST: Se envian los mails correspondientes a esos alumnos.
 
-        mensaje_email ="Por favor valida tu Padron debido a que no coincide con lo que tenemos en nuestra base de datos"
+    for mail in entregas:
+
+        mensaje_email = cuerpo
         mimeMessage = MIMEMultipart()
         mimeMessage["to"] = mail
-        mimeMessage["subject"] = "Entrega de evaluacion"  
+        mimeMessage["subject"] = asunto  
         mimeMessage.attach(MIMEText(mensaje_email, "plain"))
         decodificando_mensaje = base64.urlsafe_b64encode(mimeMessage.as_bytes()).decode()
 
         mensaje = servicio.users().messages().send(userId = "evaluaciontp2@gmail.com", body = {"raw": decodificando_mensaje}).execute()
+
+    print("Mensajes enviados! :)")
 
 
 def main():
@@ -179,7 +194,7 @@ def main():
     servicio = obtener_servicio()
     id_mails = obteniendo_ids_mails(servicio, fecha)
     datos_emails = obteniendo_datos_mails(id_mails, servicio)
-    emails_entregas_incorrectas = validando_datos_asuntos(id_mails, datos_emails)
-    enviando_mail_entregas_fallidas(servicio, emails_entregas_incorrectas)
+    emails_entregas_incorrectas = validando_padron_alumnos(id_mails, datos_emails, servicio)
+    
 
 main()
