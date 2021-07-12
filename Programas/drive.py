e75fa7f2ab2108badaa6ab4eb9168b76597b9c26
josehernandez <jhernandeza@fi.uba.ar>
2021-07-12 09:54:46 -0300

    actualizacion

--- Programas/drive.py
+++ Programas/drive.py
@@ -1,11 +1,16 @@
 import os, io
 import os.path
 
+
+
+
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import GOOGLE_API_USE_MTLS_ENDPOINT, build, Resource
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload 
+from google.auth.transport.requests import Request
+
 
 SCOPES = ['https://www.googleapis.com/auth/drive']
 
@@ -78,15 +83,14 @@ def obtener_servicio() -> Resource:
 #Disculpen que está desprolijo, cuando todo funcione bien arreglaré el formato
 
 '''
-Por el momento funciona todo de forma independiente, más adelante agrego si se quiere guardar en una carpeta el archivo o si queres mover 
-archivos entre carpetas.
-Por ahora es pura prueba y error, empirismo puro!
+Falta cosas como las busquedas anidadas y pasar a binario. Falta poquito!
+Le voy a agregar mas detalles
 '''
 
 def subir_archivo(servicio): #Sube un archivo al drive sin meterlo en alguna carpeta
     
     nombre_archivo = input('Ingrese el nombre del archivo junto con su extension (ej - imagengatito.png): ')
-    ruta_archivo = input('repita el paso anterior: ') #Lo tengo que optimizar, funciona si metes 2 veces el nombre y extension.
+    ruta_archivo = input('Ingrese donde se encuentra el archivo (ruta completa): ')
     tipo_archivo = input ('Ingrese el tipo de archivo (ej- image/png): ')
 
     archivo_metadata = {'name': nombre_archivo}
@@ -97,21 +101,26 @@ def subir_archivo(servicio): #Sube un archivo al drive sin meterlo en alguna car
     print('ID Archivo: %s' % archivo.get('id')) 
 
 
-def descargar_archivo (servicio): #aun no funciona correctamente. está copiado directamente de Google for Developers
-    
-    nombre_archivo = input('Ingrese el nombre del archivo: ')
-   
-    request = servicio.files().get_media(fileId=nombre_archivo)
-    fh = io.BytesIO()
-    downloader = MediaIoBaseDownload(fh, request)
-    done = True
-    while not done: #placeholder
-     status, done = downloader.next_chunk()
+def listar_carpetas():
+    pass
 
-     print ("Download %d%%.") % int(status.progress() * 100)
+def subir_archivo_crear_carpeta(servicio): 
+    id_carpeta = input('Ingrese el id de la carpeta: ')
+    file_metadata = {
+    'name': 'photo.jpg',
+    'parents': [id_carpeta]
+    }  
+    media = MediaFileUpload('files/photo.jpg',
+                        mimetype='image/jpeg',
+                        resumable=True)
+    file = servicio.files().create(body=file_metadata,
+                                    media_body=media,
+                                    fields='id').execute()
+    print ('File ID: %s' % file.get('id'))
 
 
-def crear_carpeta(servicio): #funciona! Voy a ver de crear una carpeta y a su vez crear archivo en dicha carpeta
+
+def crear_carpeta(servicio): #carpeta vacia
     nombre = input('Ingrese nombre de la carpeta a crear: ')
     carpeta_metadata = {
     'name': nombre,
@@ -121,13 +130,60 @@ def crear_carpeta(servicio): #funciona! Voy a ver de crear una carpeta y a su ve
     print ('ID Carpeta: %s' % file.get('id'))
 
 
+def crear_archivo(servicio): #Crea un archivo sin contenido y sin extension
+    nombre_nuevo_archivo = input('Ingrese el nombre del archivo a crear: ')
+    file_metadata = {
+    'name' : nombre_nuevo_archivo,
+    'mimeType' : 'application/vnd.google-apps.drive-sdk'
+    }
+    file = servicio.files().create(body=file_metadata,
+                                    fields='id').execute()
+    print ('File ID: %s' % file.get('id'))
+
+
+
+
+def listar_archivos(servicio, size = 10): #busquedas anidadas
+    listar = servicio.files().list(
+        pageSize=size,fields="nextPageToken, files(id, name)").execute()
+    archivos = listar.get('files', [])
+
+    if not archivos:
+        print('No se han encontrado archivos en tu Drive.')
+    else:
+        print('Estos son TODOS tus archivos con sus IDs:')
+        print ('¡También se muestran los de tu papelera!')
+        for archivo in archivos:
+            print('{0} ({1})'.format(archivo['name'], archivo['id']))
+
+
+
+def descargar_archivo(servicio): #falta pasar a binario!
+    id_archivo = input('ID del archivo -> ')
+    ruta_archivo = input ('¿Dónde lo desea guardar? -> ')
+    request = servicio.files().get_media(fileId=id_archivo)
+    fh = io.BytesIO()
+    downloader = MediaIoBaseDownload(fh, request)
+    done = False
+    while done is False:
+        status, done = downloader.next_chunk()
+        print("Download %d%%." % int(status.progress() * 100))
+    with io.open(ruta_archivo,'wb') as f:
+        fh.seek(0)
+        f.write(fh.read())
+
+
+def mover_archivo(servicio):
+    pass
 
 
 
 
 def main():
-    servicio = obtener_servicio()   
-    crear_carpeta(servicio)
+    servicio = obtener_servicio()
+    subir_archivo(servicio)
+    
+    
 
 
 main()
